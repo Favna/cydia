f3db60d3263325a6e9cb965c18495d751b53ba50
Jeroen Claassens <support@favware.tech>
2023-05-14 01:56:02 +0200

    refactor: use proper reverse dns

--- gendeb.sh
+++ gendeb.sh
@@ -19,16 +19,16 @@ run() {
     # Move into the source directory
     pushd $SOURCE_DIR
 
-    cp ../icons/* ./com.favware.darkcons/Library/Themes/Darkcons/IconBundles
-    cp ../icons/* ./com.favware.darkcons-rootless/var/jb/Library/Themes/Darkcons/IconBundles
+    cp ../icons/* ./tech.favware.darkcons/Library/Themes/Darkcons/IconBundles
+    cp ../icons/* ./tech.favware.darkcons-rootless/var/jb/Library/Themes/Darkcons/IconBundles
 
     # Build the debs
-    dpkg -b com.favware.darkcons
-    dpkg -b com.favware.darkcons-rootless
+    dpkg -b tech.favware.darkcons
+    dpkg -b tech.favware.darkcons-rootless
 
     # Move the debs to the repo/debs folder
-    mv com.favware.darkcons.deb ../debs/com.favware.darkcons_$NEW_VERSION.deb
-    mv com.favware.darkcons-rootless.deb ../debs/com.favware.darkcons-rootless_$NEW_VERSION.deb
+    mv tech.favware.darkcons.deb ../debs/tech.favware.darkcons_$NEW_VERSION.deb
+    mv tech.favware.darkcons-rootless.deb ../debs/tech.favware.darkcons-rootless_$NEW_VERSION.deb
 
     popd
     pushd $REPO_DIR
